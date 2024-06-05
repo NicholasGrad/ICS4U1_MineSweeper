265e3b5cb78382dbddf986f8c5d8132cd1fdbca2
gradni302 <gradni302@060-204-36143.hwcdsb.ca>
2024-06-04 21:23:54 -0400

    remove main class and added title Jframe

--- src/finalproject/GridLayout.java
+++ src/finalproject/GridLayout.java
@@ -12,21 +12,23 @@ import javax.swing.JPanel;
  *
  * @author nicho
  */
-public class GridLayout {
-    JFrame frame = new JFrame(); //creates frame
+public class GridLayout extends javax.swing.JFrame{
+    JFrame gridFrame = new JFrame(); //creates frame
     JButton[][] grid; //names the grid of button
     
     public GridLayout(int width, int length){ //constructor
-        frame.setLayout(new java.awt.GridLayout(width,length)); //set layout
+        gridFrame.setExtendedState(MAXIMIZED_BOTH);
+        gridFrame.setLayout(new java.awt.GridLayout(width,length)); //set layout
         grid=new JButton[width][length]; //allocate the size of grid
         for(int y=0; y<length; y++){
             for(int x=0; x<width; x++){
                     grid[x][y]=new JButton(""); //creates new button	  
-                    frame.add(grid[x][y]); //adds button to grid
+                    gridFrame.add(grid[x][y]); //adds button to grid
             }
         }
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack(); //sets appropriate size for frame
-        frame.setVisible(true); //makes frame visible
+        gridFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gridFrame.pack(); //sets appropriate size for frame
+        gridFrame.setVisible(true); //makes frame visible
     }
+    
 }
